3c12d13d1e1eec53ceb4d55e7d9e29a441cda099
jeremywang <jeremywang@thinktronltd.com>
2019-07-26 08:43:31 +0800

    first

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SplittedImage",
+    version="0.0.1",
+    author="GoatWang",
+    author_email="jeremywang@thinktronltd.com",
+    description="For splitting satellite image",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GoatWang/PySaga",
+    packages=setuptools.find_packages(),
+    package_data={'PySaga': ['saga_cmd_pkls/*']},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+
+# python3 setup.py sdist bdist_wheel
+# scp ~/Projects/PySaga/dist/PySaga-0.0.3-py3-none-any.whl  thinktron@rd.thinktronltd.com:/home/thinktron/pypi/PySaga-0.0.3-py3-none-any.whl
+# pip3 install -U --index-url http://192.168.0.167:28181/simple --trusted-host 192.168.0.167 PySaga
