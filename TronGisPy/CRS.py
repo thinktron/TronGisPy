a73b5963116f65a939e9655500bbdca1d8a16f22
davidhuang <davidhuang@thinktronltd.com>
2020-04-22 02:19:29 +0000

    Update CRS.py >> numba_transfer_group_coord_to_npidx type to INT

--- TronGisPy/CRS.py
+++ TronGisPy/CRS.py
@@ -54,7 +54,7 @@ def numba_transfer_group_coord_to_npidx(coords, geo_transform):
         x, y = coords[i][0], coords[i][1]
         x, y = __numba_transfer_coord_to_xy(x, y, *geo_transform)
         group_npidx.append((y, x))
-    return group_npidx
+    return group_npidx.astype(np.int)
 
 
 def __transfer_xy_to_coord(xy, geo_transform):
